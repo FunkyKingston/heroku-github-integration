b0d887285ab198805a5755ac33a002f7c0c70f7d
Tomas Bengtsson <tomasbengtsson83@gmail.com>
2019-11-11 13:44:11 +0200

    first commit

---
+++ web.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return 'Hello World!'
+	
+if __name__ == '__main__'
+	app.run(debug=True)
+
+# Question: Is it necessary to include app.run()? 
+# Answer: The run-command runs flask's internal web-server so the app can be tested locally. If however Apache, NGINX or some other web server loads your app it runs directly on the server
+
+# The below command will run the app (in web.py) using a Heroku 'web' dyno
+# web: FLASK_APP=web.py python -m flask run --host=0.0.0.0 --port=$PORT
